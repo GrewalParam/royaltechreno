5b39e3e6cbb08fc296ce5c5b4b0f08762fdf8129
GrewalParam <grew1748@mylaurier.ca>
2025-05-07 15:39:44 -0400

    Image

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,12 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-import type { NextConfig } from 'next'
-
-const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? '/your-repo-name' : '',
-  assetPrefix: isProd ? '/your-repo-name/' : '',
+  /* config options here */
   images: {
-    unoptimized: true, // Required for `next export` to work with <Image />
+    loader: "akamai",
+    path: "/",
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
