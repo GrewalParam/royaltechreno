61fd5160d8d5be399b0707ce0bf234d0703374ce
GrewalParam <grew1748@mylaurier.ca>
2025-05-07 15:54:52 -0400

    Image

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    loader: "akamai",
-    path: "/",
-  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/royaltechreno/' : '',
 };
 
 export default nextConfig;
